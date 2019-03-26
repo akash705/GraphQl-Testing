75b2ccb11443758d958b711154acad67ad644ea3
Akash mishra <mishraakash273694@gmail.com>
2019-03-26 19:01:24 +0530

    minro changes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,30 @@
-import a from './modules/1stModule';
-console.log(a);
-setTimeout(data=>{
-    console.log(a);
-},3000);
+import { GraphQLServer } from 'graphql-yoga';
+
+// schema
+const typeDef=`
+    type Query {
+        name : String!
+    }
+`
+
+// endpoints resolver
+const resolver ={
+    Query: {
+        name:()=>{
+            return getRandomName();
+            // return "Akash";
+        }
+    }
+}
+function getRandomName(){
+    console.log('randome name called');
+    return "Ahsk";
+}
+
+// server
+var server = new GraphQLServer({typeDefs:typeDef,resolvers : resolver});
+server.start((data,err)=>{
+    if(data){
+        console.log('server started at',data.port);
+    }
+})
