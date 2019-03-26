5de1e2551e4a39126ab449ad645fd37b077fe275
Akash mishra <mishraakash273694@gmail.com>
2019-03-26 19:03:54 +0530

    Index for es modules and index2.js for node.js modules

---
+++ src/index2.js
@@ -0,0 +1,30 @@
+var {GraphQLServer} = require('graphql-yoga');
+
+// schema
+const typeDef=`
+    type Query {
+        name : String!
+    }
+`
+
+// endpoints resolver
+const resolver ={
+    Query: {
+        name:()=>{
+            return getRandomName();
+            // return "Akash";
+        }
+    }
+}
+function getRandomName(){
+    console.log('randome name called');
+    return "Ahsk";
+}
+
+// server
+var server = new GraphQLServer({typeDefs:typeDef,resolvers : resolver});
+server.start((data,err)=>{
+    if(data){
+        console.log('server started at',data.port);
+    }
+})
